181cce3117c9f75f59e3c859fbfe676127113d16
masumraj <promotionbd321@gmail.com>
2025-04-29 08:20:36 +0600

    sign up screen create

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,71 +1,6 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-//
-// class LoginScreen extends StatefulWidget {
-//   const LoginScreen({super.key});
-//
-//   @override
-//   State<LoginScreen> createState() => _LoginScreenState();
-// }
-//
-// class _LoginScreenState extends State<LoginScreen> {
-//   final TextEditingController emailController = TextEditingController();
-//   final TextEditingController passwordController = TextEditingController();
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//
-//   Future<void> signIn() async {
-//     try {
-//       await _auth.signInWithEmailAndPassword(
-//         email: emailController.text.trim(),
-//         password: passwordController.text.trim(),
-//       );
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text('Login Successful')),
-//       );
-//     } catch (e) {
-//       print(e);
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text('Login Failed')),
-//       );
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Firebase Login'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             TextField(
-//               controller: emailController,
-//               decoration: const InputDecoration(labelText: 'Email'),
-//             ),
-//             const SizedBox(height: 16),
-//             TextField(
-//               controller: passwordController,
-//               decoration: const InputDecoration(labelText: 'Password'),
-//               obscureText: true,
-//             ),
-//             const SizedBox(height: 24),
-//             ElevatedButton(
-//               onPressed: signIn,
-//               child: const Text('Sign In'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -134,10 +69,14 @@ class _SignUpScreenState extends State<SignUpScreen> {
             const SizedBox(height: 12),
             TextButton(
               onPressed: () {
-                Navigator.pop(context); // Go back to SignInScreen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()),
+                );
               },
               child: const Text('Already have an account? Sign In'),
             ),
+
           ],
         ),
       ),
